c6b0a74df4f2f1321616d1c17dd10568d3c8b882
Marcel <fleuren.marcel@gmail.com>
2019-01-21 10:42:56 +0100

    Updates

---
+++ precache-manifest.258642859e610e776312bd205a5683a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "81058b3dddc03efadba5bd6425ef974f",
+    "url": "/Techdemo/ReactApp/static/media/backgroundImage.81058b3d.png"
+  },
+  {
+    "revision": "b13c71756641c0ed423c9c116bd4e430",
+    "url": "/Techdemo/ReactApp/static/media/Oval.b13c7175.png"
+  },
+  {
+    "revision": "29e53ef63ce5ccfcc3b7",
+    "url": "/Techdemo/ReactApp/static/js/runtime~main.29e53ef6.js"
+  },
+  {
+    "revision": "09115cad8c78b4539264",
+    "url": "/Techdemo/ReactApp/static/js/main.09115cad.chunk.js"
+  },
+  {
+    "revision": "20f496db89c281fefafe",
+    "url": "/Techdemo/ReactApp/static/js/1.20f496db.chunk.js"
+  },
+  {
+    "revision": "09115cad8c78b4539264",
+    "url": "/Techdemo/ReactApp/static/css/main.eb67c60f.chunk.css"
+  },
+  {
+    "revision": "a54a15271f7848b61238ba1fea4dc6cc",
+    "url": "/Techdemo/ReactApp/index.html"
+  }
+];
